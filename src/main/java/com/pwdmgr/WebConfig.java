406c99d30d4f53266e0f83006940b0bdbe91ef37
KrishnaBhawsar <krishnabhawsar8@gmail.com>
2024-04-06 22:37:50 +0530

    updating properties file 2

--- src/main/java/com/pwdmgr/WebConfig.java
+++ src/main/java/com/pwdmgr/WebConfig.java
@@ -7,13 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Value("${client.url}")
-    private String clientUrl;
+    @Value("${client.url.1}")
+    private String clientUrl1;
+    @Value("&{client.url.2}")
+    private String clientUrl2;
 
     public void addCorsMappings(CorsRegistry registry) {
-        System.out.println(clientUrl);
+        System.out.println(clientUrl1+ clientUrl2);
         registry.addMapping("/**")
-                .allowedOrigins(clientUrl) // Update with your client's URL
+                .allowedOrigins(clientUrl1, clientUrl2) // Update with your client's URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
